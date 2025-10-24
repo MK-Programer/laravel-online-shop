3c11958705fd4841a27cecb8d584cbdba3195875
mustafakhaleedd <mustafakhaleedd@gmail.com>
2025-10-24 20:25:00 +0300

    refactor login structure

--- app/Http/Controllers/Admin/Auth/AdminLoginController.php
+++ app/Http/Controllers/Admin/Auth/AdminLoginController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Admin;
+namespace App\Http\Controllers\Admin\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
@@ -11,7 +11,7 @@ class AdminLoginController extends Controller
 {
     public function index()
     {
-        return view('admin.login');
+        return view('admin.auth.login');
     }
 
     public function authenticate(Request $request)
