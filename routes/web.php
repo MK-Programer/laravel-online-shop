248282d32a0b07534851f44c13302acd868a88b5
mustafakhaleedd <mustafakhaleedd@gmail.com>
2025-10-24 10:18:24 +0300

    create admin login interface

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminLoginController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('admin')->group(function(){
+    Route::get('login', [AdminLoginController::class, 'index'])->name('admin.login');
+});
+
+
