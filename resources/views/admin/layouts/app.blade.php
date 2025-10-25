86e9c212697202a2f2315aa173ba768dfa4ff484
mustafakhaleedd <mustafakhaleedd@gmail.com>
2025-10-25 20:05:33 +0300

    modify loading ui structure

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -24,14 +24,7 @@
 			<!-- Content Wrapper. Contains page content -->
 			<div class="content-wrapper">
 				<!-- Global Page Loader -->
-				<div id="page_loader"
-					style="position:fixed;top:0;left:0;width:100%;height:100%;
-							background:transparent;z-index:9999;
-							display:flex;justify-content:center;align-items:center;">
-					<div class="spinner-border text-primary" role="status">
-						<span class="visually-hidden">Loading...</span>
-					</div>
-				</div>
+				@include('admin.layouts.loading')
                 @yield('content')
 			</div>
 			<!-- /.content-wrapper -->
---
+++ resources/views/admin/layouts/loading.blade.php
@@ -0,0 +1,9 @@
+<!-- Global Page Loader -->
+<div id="page_loader"
+    style="position:fixed;top:0;left:0;width:100%;height:100%;
+							background:transparent;z-index:9999;
+							display:flex;justify-content:center;align-items:center;">
+    <div class="spinner-border text-primary" role="status">
+        <span class="visually-hidden">Loading...</span>
+    </div>
+</div>
