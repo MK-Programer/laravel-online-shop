2f4703c44dc509293ff7dc744c6707d2b0fcc266
mustafakhaleedd <mustafakhaleedd@gmail.com>
2025-10-30 13:18:47 +0300

    create Brand model, factory, and seeder for brand data management

---
+++ database/factories/BrandFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Brand>
+ */
+class BrandFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $name = fake()->unique()->name();
+        $slug = str()->slug($name);
+        return [
+            'name' => $name,
+            'slug' => $slug,
+            'status' => fake()->boolean(),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,7 +21,8 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             // CategorySeeder::class,
-            SubCategorySeeder::class
+            // SubCategorySeeder::class,
+            BrandSeeder::class,
         ]);
     }
 }
