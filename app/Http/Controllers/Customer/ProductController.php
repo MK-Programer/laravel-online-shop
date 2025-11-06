85c1868df8aeafb43749877eee7948705e6add79
mustafakhaleedd <mustafakhaleedd@gmail.com>
2025-11-06 22:43:31 +0200

    add getPriceRanges and getProducts methods

--- app/Http/Controllers/Customer/ProductController.php
+++ app/Http/Controllers/Customer/ProductController.php
@@ -9,7 +9,8 @@ use Illuminate\Http\Request;
 class ProductController extends Controller
 {
     private $featuredProductsHomeLimit = 8,
-        $latestProductsHomeLimit = 8;
+        $latestProductsHomeLimit = 8,
+        $priceRangesSteps = 4;
 
 
     public function getHomeFeaturedProducts()
@@ -32,6 +33,43 @@ class ProductController extends Controller
             ->take($this->latestProductsHomeLimit)
             ->orderByDesc('id')
             ->orderBy('title')
+            ->get();
+    }
+
+    public function getPriceRanges()
+    {
+        $min = Product::where('status', 1)->min('price'); // 22.73
+        $max = Product::where('status', 1)->max('price'); // 488.23
+
+        if($min == null || $max == null) return []; // no products
+
+        $interval = ceil(($max - $min) / $this->priceRangesSteps); //116
+
+        $ranges = [];
+
+        for($i = 0; $i < $this->priceRangesSteps; $i++)
+        {
+            $from = $min + ($i * $interval);
+            $to = $from + $interval - 1;
+
+            $count = Product::where('status', 1)
+                ->whereBetween('price', [$from, $to])
+                ->count();
+
+            $ranges[] = [
+                'label' => config('app.currency')." {$from} - {$to}",
+                'count' => $count,
+                'min' => $from,
+                'max' => $to,
+            ];
+        }
+        return $ranges;
+    }
+
+    public function getProducts()
+    {
+        return Product::where('status', 1)
+            ->orderByDesc('id')
             ->get();
     }
 }
