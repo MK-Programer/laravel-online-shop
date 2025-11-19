a9d57945bbf87609ee7a16504e88446d4311ecbe
mustafakhaleedd <mustafakhaleedd@gmail.com>
2025-11-19 20:06:33 +0200

    replace button with link to avoid bootstrap versioning conflict

--- resources/views/partials/flash.blade.php
+++ resources/views/partials/flash.blade.php
@@ -1,14 +1,14 @@
 @if(Session::has('success'))
     <div class="alert alert-success alert-dismissible">
-        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+        <a href="javascript:void(0);" class="close text-decoration-none" aria-hidden="true">×</a>
         <h4><i class="icon fa fa-check"></i> Success!</h4>
-        {{ Session::get('success')  }}
+        {{ Session::get('success') }}
     </div>
 @endif
 
 @if(Session::has('error'))
     <div class="alert alert-danger alert-dismissible">
-        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+        <a href="javascript:void(0);" class="close text-decoration-none" aria-hidden="true">×</a>
         <h4><i class="icon fa fa-ban"></i> Error!</h4>
        {{ Session::get('error')  }}
     </div>
