fb08669bf2a314e9b008f14133cf2016d9924fe4
mustafakhaleedd <mustafakhaleedd@gmail.com>
2025-11-20 16:32:02 +0200

    update header alignment to text-start

--- resources/views/partials/flash.blade.php
+++ resources/views/partials/flash.blade.php
@@ -1,7 +1,7 @@
 @if(Session::has('success'))
     <div class="alert alert-success alert-dismissible">
         <a href="javascript:void(0);" class="close text-decoration-none" aria-hidden="true">×</a>
-        <h4><i class="icon fa fa-check"></i> Success!</h4>
+        <h4 class="text-start"><i class="icon fa fa-check"></i> Success!</h4>
         {{ Session::get('success') }}
     </div>
 @endif
@@ -9,7 +9,7 @@
 @if(Session::has('error'))
     <div class="alert alert-danger alert-dismissible">
         <a href="javascript:void(0);" class="close text-decoration-none" aria-hidden="true">×</a>
-        <h4><i class="icon fa fa-ban"></i> Error!</h4>
+        <h4 class="text-start"><i class="icon fa fa-ban"></i> Error!</h4>
        {{ Session::get('error')  }}
     </div>
 @endif
