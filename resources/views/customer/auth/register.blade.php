be7ba55d422498e323de2ff4f2ab98de44ee7a05
mustafakhaleedd <mustafakhaleedd@gmail.com>
2025-11-20 16:34:20 +0200

    handle login now route

--- resources/views/customer/auth/register.blade.php
+++ resources/views/customer/auth/register.blade.php
@@ -39,7 +39,7 @@
                     </div>
                     <button type="submit" class="btn btn-dark btn-block btn-lg" value="Register">Register</button>
                 </form>
-                <div class="text-center small">Already have an account? <a href="login.php">Login Now</a></div>
+                <div class="text-center small">Already have an account? <a href="{{ route('customer.login') }}">Login Now</a></div>
             </div>
         </div>
     </section>
