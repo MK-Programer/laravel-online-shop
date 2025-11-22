f34fd361233f5b1cfea4b81cf11c2aba4181d758
mustafakhaleedd <mustafakhaleedd@gmail.com>
2025-11-22 21:26:33 +0200

    handle alignment

--- resources/views/partials/flash.blade.php
+++ resources/views/partials/flash.blade.php
@@ -1,15 +1,15 @@
 @if(Session::has('success'))
-    <div class="alert alert-success alert-dismissible">
-        <a href="javascript:void(0);" class="close text-decoration-none" aria-hidden="true">×</a>
-        <h4 class="text-start"><i class="icon fa fa-check"></i> Success!</h4>
+    <div class="alert alert-success alert-dismissible text-start">
+        <a href="javascript:void(0);" class="close text-decoration-none" style="float: right;" aria-hidden="true">×</a>
+        <h4><i class="icon fa fa-check"></i> Success!</h4>
         {{ Session::get('success') }}
     </div>
 @endif
 
 @if(Session::has('error'))
-    <div class="alert alert-danger alert-dismissible">
-        <a href="javascript:void(0);" class="close text-decoration-none" aria-hidden="true">×</a>
-        <h4 class="text-start"><i class="icon fa fa-ban"></i> Error!</h4>
+    <div class="alert alert-danger alert-dismissible text-start">
+        <a href="javascript:void(0);" class="close text-decoration-none" style="float: right;" aria-hidden="true">×</a>
+        <h4><i class="icon fa fa-ban"></i> Error!</h4>
        {{ Session::get('error')  }}
     </div>
 @endif
